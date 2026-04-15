291fccfdd682fdbdb6bb36d3534555d6a344b12f
nareio <much.lio@laposte.net>
2026-04-15 21:31:02 +0200

    first commit

---
+++ src/main/java/org/example/Personne.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import java.util.Random;
+
+public class Personne {
+    private int identifiant;
+    private String nom;
+    private String adresse;
+    private Personne contact;
+
+
+    public Personne(String nom, String adresse, org.example.Personne contact) {
+        this.identifiant = new Random().nextInt(1_000_000);
+        this.nom = nom;
+        this.adresse = adresse;
+        this.contact = contact;
+    }
+
+
+}
